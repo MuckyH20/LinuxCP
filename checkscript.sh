4d5f376a789490666322937d4883f0a12a2a9985
MuckyH20 <187213721+MuckyH20@users.noreply.github.com>
2024-12-12 20:43:03 -0500

    Update checkscript.sh

--- checkscript.sh
+++ checkscript.sh
@@ -1043,20 +1043,20 @@ function securityAuditing() {
 
 function media() {
     echo "Removing media files...\n"
-    find / -name '*.mp3' -type f -delete
-    find / -name '*.mov' -type f -delete
-    find / -name '*.mp4' -type f -delete
-    find / -name '*.avi' -type f -delete
-    find / -name '*.mpg' -type f -delete
-    find / -name '*.mpeg' -type f -delete
-    find / -name '*.flac' -type f -delete
-    find / -name '*.m4a' -type f -delete
-    find / -name '*.flv' -type f -delete
-    find / -name '*.ogg' -type f -delete
-    find /home -name '*.gif' -type f -delete
-    find /home -name '*.png' -type f -delete
-    find /home -name '*.jpg' -type f -delete
-    find /home -name '*.jpeg' -type f -delete
+    find / -name '*.mp3' -type f -exec rm -i {} \;
+    find / -name '*.mov' -type f -exec rm -i {} \;
+    find / -name '*.mp4' -type f -exec rm -i {} \;
+    find / -name '*.avi' -type f -exec rm -i {} \;
+    find / -name '*.mpg' -type f -exec rm -i {} \;
+    find / -name '*.mpeg' -type f -exec rm -i {} \;
+    find / -name '*.flac' -type f -exec rm -i {} \;
+    find / -name '*.m4a' -type f -exec rm -i {} \;
+    find / -name '*.flv' -type f -exec rm -i {} \;
+    find / -name '*.ogg' -type f -exec rm -i {} \;
+    find /home -name '*.gif' -type f -exec rm -i {} \;
+    find /home -name '*.png' -type f -exec rm -i {} \;
+    find /home -name '*.jpg' -type f -exec rm -i {} \;
+    find /home -name '*.jpeg' -type f -exec rm -i {} \;
     }
 
 
